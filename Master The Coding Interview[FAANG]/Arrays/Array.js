59811d455ac7a8a14f57179634e69157eb6807ab
ezikechris <christophermbe1009@gmail.com>
2021-02-23 17:29:08 +0100

    add Linear Time & Constant time BigO

--- Master The Coding Interview[FAANG]/Arrays/Array.js
+++ Master The Coding Interview[FAANG]/Arrays/Array.js
@@ -4,6 +4,16 @@
 ///MOBVEMENT OF POINTERS BASED ON A SPECIFIC LOGIC
 
 
+
+
+//// THE FIRST BIG O notation /////
+
+///Linear Time///// or O of n O(n)
+
+// O(n) the number of inputs and the time it takes the computer to carryout those inputs///
+/// compering the size of the inputs and number of operations////
+/// as the operations gets larger those the code scale/////
+
 const everyone = ['chris', 'drake', 'micheal', 'james', 'bobby'];
 
 function findJames(array){
@@ -14,11 +24,18 @@ function findJames(array){
 }
 
 
-//// THE FIRST BIG O notation /////
 
-///Linear Time/////
+//// Constant Time //// 0(1)/// ONLY RUNS BASED ON THE CONSTANT AMOUNT OF TIME 0(1)
+const boxes = [1,2, 3,4,5,];
 
-// O(n) the number of inputs and the time it takes the computer to carryout those inputs///
-/// compering the size of the inputs and number of operations////
-/// as the operations gets larger those the code scale/////
+function groupBoxes(boxes) {
+    console.log(boxes[0])
+    console.log(boxes[1])
+}
+
+groupBoxes(boxes);
+
+//// IT WILL ALWAYS SCALE ///
+
+
 
