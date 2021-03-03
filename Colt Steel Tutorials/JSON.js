eb7560aa3d52abac2dce6c2098b2523c1cea7968
ezikechris <christophermbe1009@gmail.com>
2021-03-03 12:37:46 +0100

    add apis ajax json

---
+++ Colt Steel Tutorials/JSON.js
@@ -0,0 +1,24 @@
+/// JSON JAVASCRIPT OBJECT NOTATION ///
+
+/// REPLACED XML EXTENSIBLE MARk-UP LANGUAGE ///
+
+
+///          JSON SIMPLE IS A FORMAT FOR SENDING DATA FROM ONE APPLICATION OR USER TO ANOTHER ..         .///
+
+// CONVERTING JSON FILE OR TEXT TO A JAVASCRIPT FIL AND OPPOSITE
+
+/// JSON.Parse 
+
+/// this converts the json file to  the javascriptfile//
+
+eg json.parse(parses the data inside this bracket)
+
+
+///JSON.stringify parses javascript files into json files///
+
+json.stringify(parse the data inside the brackets)
+
+
+
+
+
--- Master The Coding Interview[FAANG]/Arrays/Array.js
+++ Master The Coding Interview[FAANG]/Arrays/Array.js
@@ -42,3 +42,8 @@ groupBoxes(boxes);
 
 
 
+
+
+
+
+
