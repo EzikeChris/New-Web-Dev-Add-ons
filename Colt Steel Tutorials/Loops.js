e60ac3d745e2e345938f33411509ed8afad0691b
ezikechris <christophermbe1009@gmail.com>
2021-04-16 13:43:52 +0100

    aded Loops

---
+++ Colt Steel Tutorials/Loops.js
@@ -0,0 +1,25 @@
+/// FOR LOOPS IN JAVASCRIPT ////
+for (let i = 1; i < 5; i++) {
+  // intialExpression let i =1
+  // Condtion i <= 5
+  // IncrementExpression i++
+  console.log(i);
+}
+
+for (let i = 0; i <= 20; i += 2) {
+  console.log(i);
+}
+// PRINTS OUT EVEN NUMBERS FROM 2
+
+for (let i = 100; i >= 0; i -= 10) {
+  console.log(i);
+}
+// prints out numbers decrementally from  100
+
+// INFINITE LOOPS IN JAVASCRIPT//
+
+for (let i = 20; i >= 0; i++) {
+  console.log(i);
+}
+
+/// LOOPING OVER ARRRAYS ///
